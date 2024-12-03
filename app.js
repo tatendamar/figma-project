e045a21083e976f6bdaf61b450ab9102615894df
tmarufu <tatenda.marufu@ecocashholdings.co.zw>
2024-12-03 10:01:12 +0200

    fix sidebar on mobile

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector('.mobile-nav');
 
 window.addEventListener("scroll", function() {
-    if (window.scrollY > 60) {
+    if (window.scrollY > 10) {
         nav.classList.add('scrolled');
     } else {
         nav.classList.remove("scrolled");
